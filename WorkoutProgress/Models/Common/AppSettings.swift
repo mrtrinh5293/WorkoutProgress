b13526b9d0a8eba9ee5f3ca2e67695249ce888d1
duc <ductest@mackintoshsmbp4.lan>
2022-01-16 12:33:40 -0500

    configuaration on SceneDelegate in order to fix duplicate symbol architecture

--- WorkoutProgress/Models/Common/AppSettings.swift
+++ WorkoutProgress/Models/Common/AppSettings.swift
@@ -9,13 +9,14 @@ import Foundation
 import SwiftUI
 
 class AppSettings: ObservableObject {
-    
+    //
     init() {
         self.selectedTabs = UserDefaults.standard.value(forKey: "selectedTab") as? Int ?? 0
         self.userName = UserDefaults.standard.value(forKey: "userName") as? String ?? "username"
     }
     
     @Published var selectedTabs: Int {
+        // we use either didSet to execute code when a property has just been set, or willSet to execute code before a property has been set.
         didSet {
             UserDefaults.standard.set(selectedTabs, forKey: "selectedTab")
         }
